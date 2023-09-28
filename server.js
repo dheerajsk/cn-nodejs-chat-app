0120654690c3d1a45b4831e63e3cf43f75b5c1fd
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-09-28 17:12:04 +0530

    user identification

--- server.js
+++ server.js
@@ -20,9 +20,20 @@ const io = new Server(server,{
 
 io.on('connection', (socket)=>{
     console.log("Connection is established")
+
+    socket.on("join", (data)=>{
+        socket.username=data;
+    })
+
     socket.on('new_message', (message)=>{
+
+        let userMessage = {
+            username: socket.username,
+            message: message
+        }
+
         // broadcacst this message to all the clients.
-        socket.broadcast.emit('broadcast_message', message);
+        socket.broadcast.emit('broadcast_message', userMessage);
     })
     socket.on('disconnect', ()=>{
         console.log("Connection is disconnected");
